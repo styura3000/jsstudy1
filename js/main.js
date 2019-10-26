16fb3fcde9d41f7e689d3ee94ab4e0feab35c0a3
styura3000 <styura3000@gmail.com>
2019-10-26 21:58:20 +0300

    end of the day (261019)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,64 @@
-"use strict";
-let name = "Ilya";
+let array=[1,3,10,69];
+console.log(array[3]);
 
-alert( `hello ${1}` ); // ?
+/*
+function watch(massage) {
+    return ("Этот текст добавлен к меседжу - " + massage);
+}
 
-alert( `hello ${"name"}` ); // ?
+let massage=watch("Привет как дела");
+console.log(massage);
+*/
 
-alert( `hello ${name}` ); // ?
+
+
+
+
+
+/*
+let car = {
+    color:"red",
+    maxSpeed:250
+    }
+
+
+
+console.log(car.color);
+
+let a123=colorChange;
+a123();
+
+console.log(car.color);
+
+
+function colorChange(){
+    car.color="green";
+
+}
+*/
+
+
+/*
+let car2={
+    color:"green",
+    maxSpeed:250
+}
+
+console.log("Car: "+car.color);
+console.log("Car2: "+car2.color);
+
+let theColor="black";//например ее можно запросить у пользователя
+
+colorChange(car,theColor);
+colorChange(car2,theColor);
+
+console.log("Car: "+car.color);
+console.log("Car2: "+car2.color);
+
+
+function colorChange(theCar,theColor) {
+    theCar.color=`${theColor}`;
+}
+*/
+
+

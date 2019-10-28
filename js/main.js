4d36636b3c921ad5901cb19a92503416e6d23ada
styura3000 <styura3000@gmail.com>
2019-10-28 18:18:44 +0300

    проработали классы и циклы

--- js/main.js
+++ js/main.js
@@ -1,61 +1,16 @@
-let array=[1,3,10,69];
-console.log(array[3]);
-
-/*
-function watch(massage) {
-    return ("Этот текст добавлен к меседжу - " + massage);
-}
-
-let massage=watch("Привет как дела");
-console.log(massage);
-*/
-
-
-
-
-
-
-/*
-let car = {
-    color:"red",
-    maxSpeed:250
+class Hater{
+    constructor(firstName, lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        }
+        sayName(){
+        console.log(`${this.firstName} ${this.lastName}`);
     }
-
-
-
-console.log(car.color);
-
-let a123=colorChange;
-a123();
-
-console.log(car.color);
-
-
-function colorChange(){
-    car.color="green";
-
-}
-*/
-
-
-/*
-let car2={
-    color:"green",
-    maxSpeed:250
 }
 
-console.log("Car: "+car.color);
-console.log("Car2: "+car2.color);
-
-let theColor="black";//например ее можно запросить у пользователя
-
-colorChange(car,theColor);
-colorChange(car2,theColor);
-
-console.log("Car: "+car.color);
-console.log("Car2: "+car2.color);
+let hater1=new Hater("Вася", "Пупкин");
+hater1.sayName();
+console.log(hater1);
 
 
-alert( `hello ${"name"}` ); // ?
 
-alert( `hello ${name}` ); // ?

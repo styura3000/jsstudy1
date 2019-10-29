dad2cdbbde15edc7a7c67f189b19a63e6cc76906
styura3000 <styura3000@gmail.com>
2019-10-29 20:02:21 +0300

    end of the day (291019)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-class Hater{
-    constructor(firstName, lastName){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        }
-        sayName(){
-        console.log(`${this.firstName} ${this.lastName}`);
+class Task {
+    constructor(title=''){
+        this.title=title;
+        this.done=false;
+        Task.count+=1;
+        console.log('Сгенерирована новая задача');
+    }
+    complete(){
+        this.done=true;
+        console.log(`Задача ${this.title} - Выполнена`);
     }
 }
+Task.count=0;
+let task1=new Task('Убрать комнату');
+let task2=new Task('помой машину тварь!!!');
 
-let hater1=new Hater("Вася", "Пупкин");
-hater1.sayName();
-console.log(hater1);
+
+console.log(task1.title);
+console.log(task1.done);
+console.log(task2.title);
+console.log(task2.done);
+
+task2.complete();
+console.log(task2.done);
+
+console.log(Task.count);
 
 
 

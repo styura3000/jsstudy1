d4169fc68ada67a1d66c53d0247ddba022d7d852
styura3000 <styura3000@gmail.com>
2019-10-31 18:10:10 +0300

    end of the day 311019

--- js/main.js
+++ js/main.js
@@ -1,29 +1,8 @@
-class Task {
-    constructor(title=''){
-        this.title=title;
-        this.done=false;
-        Task.count+=1;
-        console.log('Сгенерирована новая задача');
-    }
-    complete(){
-        this.done=true;
-        console.log(`Задача ${this.title} - Выполнена`);
-    }
+"use strict";
+
+function slowScroll(id) {
+    let offset=0;
+    $('html, body').animate({scrollTop:$(id).offset().top-offset}, 1000);
+    return false;
 }
-Task.count=0;
-let task1=new Task('Убрать комнату');
-let task2=new Task('помой машину тварь!!!');
-
-
-console.log(task1.title);
-console.log(task1.done);
-console.log(task2.title);
-console.log(task2.done);
-
-task2.complete();
-console.log(task2.done);
-
-console.log(Task.count);
-
-
 

fa5d05d7238c7f8f0b834e3b96e461d58a87147d
styura3000 <styura3000@gmail.com>
2019-11-01 18:22:44 +0300

    end of the day (311019)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 "use strict";
 
-function slowScroll(id) {
-    let offset=0;
-    $('html, body').animate({scrollTop:$(id).offset().top-offset}, 1000);
-    return false;
-}
+let x=6;
+let y=5;
+let bool=true;
 
+if(x>y && bool){document.write('x > y')}
+    else if(x<y){document.write('x < y')}
+    else if(x===y){document.write('x = y')}
+    else {document.write('Что то не то с переменными')}
+document.write('</br>Ушел дальше...</br></br>');
+/*
+let result='1';
+switch (result) {
+    case 'a': document.write('result = 1'); break;
+    case 'b': document.write('result = 2'); break;
+    case 'c': document.write('result = 3'); break;
+    case 'd': document.write('result = 4'); break;
+    default: document.write('result неопределен')
+}
+document.write('</br>');
+
+x>y ? document.write('yes') : document.write('no');*/

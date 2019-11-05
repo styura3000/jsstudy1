78fb3ddaad2860e3ccdce99f018c7ffbb42de9e7
styura3000 <styura3000@gmail.com>
2019-11-05 18:22:04 +0300

    end of the day (051119)

--- js/main.js
+++ js/main.js
@@ -1,23 +1,9 @@
 "use strict";
 
-let x=6;
-let y=5;
-let bool=true;
+let person;
+person=prompt('Как тебя зовут, дружек?');
+alert(`Здравствуй ${person}`);
+if (confirm(`Тебя ведь зовут ${person}? Не так ли?`)){
+    alert(`Здравствуй ${person} еще раз...`);
 
-if(x>y && bool){document.write('x > y')}
-    else if(x<y){document.write('x < y')}
-    else if(x===y){document.write('x = y')}
-    else {document.write('Что то не то с переменными')}
-document.write('</br>Ушел дальше...</br></br>');
-/*
-let result='1';
-switch (result) {
-    case 'a': document.write('result = 1'); break;
-    case 'b': document.write('result = 2'); break;
-    case 'c': document.write('result = 3'); break;
-    case 'd': document.write('result = 4'); break;
-    default: document.write('result неопределен')
-}
-document.write('</br>');
-
-x>y ? document.write('yes') : document.write('no');*/
+}else alert("А зачем же ты меня обманул в первый раз....");

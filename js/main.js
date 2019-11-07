9028772d1246422579e88f2a284731a7bbc409ae
styura3000 <styura3000@gmail.com>
2019-11-07 18:26:59 +0300

    end of the day 071119

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 "use strict";
 
-function buttonClick() {
-    alert('Ты все таки ее нажал, красавчик :)')
-}
-let i=0;
-function counterMouseAiming(element1) {
-    i++;
-    element1.innerHTML=`На этот объект было наведено ${i} раз`;
+function valid(formData) {
+    let fail=false;
+    let name=formData.name.value;
+    let password=formData.password.value;
+    let rePassword=formData.rePassword.value;
+    let state=formData.state.value;
+    let eMail=formData.eMail.value;
+
+    if (name==="" || name===" " || name==="  ") {
+        fail="Вы ввели неверное имя";
+    }
+    else if (eMail==="" || eMail===" " || eMail==="  ") {
+        fail="Вы ввели неверный адрес электронной почты";
+
+    }    else if (password==="" || password===" " || password==="  ") {
+        fail="Вы ввели неверный пароль";
+    }
+    else if (rePassword!==password) {
+        fail="Вы ввели неверное подтверждение пароля";
+    }
+    else if (state==="") {
+        fail="введите пол";
+    }
+
+
+    if (fail) {alert(fail)} else window.location='http://ya.ru';
+
 }

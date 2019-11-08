8c0b54213f262884e658c815ff42249be9bf37d2
styura3000 <styura3000@gmail.com>
2019-11-08 18:19:56 +0300

    end of the day (081119)

--- js/main.js
+++ js/main.js
@@ -1,30 +1,20 @@
 "use strict";
+// let date=new Date();
 
-function valid(formData) {
-    let fail=false;
-    let name=formData.name.value;
-    let password=formData.password.value;
-    let rePassword=formData.rePassword.value;
-    let state=formData.state.value;
-    let eMail=formData.eMail.value;
+// let text=`Год сейчас - ${date.getFullYear()}`;
+// text += `\nМесяц сейчас - ${date.getMonth()}`;
+// text += `\nДата сейчас - ${date.getDate()}`;
+// text += `\nЧасов сейчас - ${date.getHours()}`;
+// text += `\nМинут сейчас - ${date.getMinutes()}`;
+// text += `\nСекунд сейчас - ${date.getSeconds()}`;
+// alert(text);
 
-    if (name==="" || name===" " || name==="  ") {
-        fail="Вы ввели неверное имя";
-    }
-    else if (eMail==="" || eMail===" " || eMail==="  ") {
-        fail="Вы ввели неверный адрес электронной почты";
-
-    }    else if (password==="" || password===" " || password==="  ") {
-        fail="Вы ввели неверный пароль";
-    }
-    else if (rePassword!==password) {
-        fail="Вы ввели неверное подтверждение пароля";
-    }
-    else if (state==="") {
-        fail="введите пол";
-    }
-
-
-    if (fail) {alert(fail)} else window.location='http://ya.ru';
-
-}
+let myDate=new Date(1999, 10,12, 14,32,10);
+myDate.setFullYear(1750);
+let text=`Год сейчас - ${myDate.getFullYear()}`;
+text += `\nМесяц сейчас - ${myDate.getMonth()}`;
+text += `\nДата сейчас - ${myDate.getDate()}`;
+text += `\nЧасов сейчас - ${myDate.getHours()}`;
+text += `\nМинут сейчас - ${myDate.getMinutes()}`;
+text += `\nСекунд сейчас - ${myDate.getSeconds()}`;
+alert(text);

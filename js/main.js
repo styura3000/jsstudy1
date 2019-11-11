e7845af810ccecb4276b0ad53e782b25bec73e1f
styura3000 <styura3000@gmail.com>
2019-11-11 18:24:17 +0300

    end of the day 111119

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
 "use strict";
-// let date=new Date();
 
-// let text=`Год сейчас - ${date.getFullYear()}`;
-// text += `\nМесяц сейчас - ${date.getMonth()}`;
-// text += `\nДата сейчас - ${date.getDate()}`;
-// text += `\nЧасов сейчас - ${date.getHours()}`;
-// text += `\nМинут сейчас - ${date.getMinutes()}`;
-// text += `\nСекунд сейчас - ${date.getSeconds()}`;
-// alert(text);
-
-let myDate=new Date(1999, 10,12, 14,32,10);
-myDate.setFullYear(1750);
-let text=`Год сейчас - ${myDate.getFullYear()}`;
-text += `\nМесяц сейчас - ${myDate.getMonth()}`;
-text += `\nДата сейчас - ${myDate.getDate()}`;
-text += `\nЧасов сейчас - ${myDate.getHours()}`;
-text += `\nМинут сейчас - ${myDate.getMinutes()}`;
-text += `\nСекунд сейчас - ${myDate.getSeconds()}`;
-alert(text);
+let string="Небольшая строка";
+let text;
+text="Количество символов в даннной строке - " + string.length + "</br>";
+text+=`Строка в верхнем регистре - ${string.toUpperCase()}</br>`;
+text+=`Строка в нижнем регистре - ${string.toLocaleLowerCase()}</br>`;
+text+=`Обрезание - ${string.substring(0,9)}</br>`;
+text+=`3-й символ строки - ${string.charAt(3)}</br>`;
+text+=`С какого по порядку индекса начинается "стро" - ${string.indexOf("стро")}</br>`;
+document.write(text);

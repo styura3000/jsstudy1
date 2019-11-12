aa971a36a5abcb6d4180de82fe83575fefd6123a
styura3000 <styura3000@gmail.com>
2019-11-12 18:48:53 +0300

    end of the day (121119)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-"use strict";
+let message;
+let login='Директор';
 
-let string="Небольшая строка";
-let text;
-text="Количество символов в даннной строке - " + string.length + "</br>";
-text+=`Строка в верхнем регистре - ${string.toUpperCase()}</br>`;
-text+=`Строка в нижнем регистре - ${string.toLocaleLowerCase()}</br>`;
-text+=`Обрезание - ${string.substring(0,9)}</br>`;
-text+=`3-й символ строки - ${string.charAt(3)}</br>`;
-text+=`С какого по порядку индекса начинается "стро" - ${string.indexOf("стро")}</br>`;
-document.write(text);
+(login === 'Сотрудник')
+                        ? message = 'Привет':(login === 'Директор')
+                        ? message = 'Здравствуйте': (login === '')
+                        ? message = 'Нет логина': message = '';
+document.write(message);
